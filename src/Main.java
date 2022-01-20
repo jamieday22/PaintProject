356e7d6daaf1d16023cf51441434a4e1636f00e9
jamieday22 <jamierobertday@hotmail.com>
2022-01-20 19:36:42 +0000

    Change simple

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,82 @@
+import java.net.StandardSocketOptions;
 import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
 
-        float randomNumberGenerator = (float)(Math.random() * 20);
+        //float randomNumberGenerator = (float)(Math.random() * 20);
+        float roomHeightWall1;
+        float roomWidthWall1;
+        float ceilingWidth;
+        float ceilingHeight;
+        float doorHeight;
+        float doorWidth;
+        float windowHeight;
+        float windowWidth;
+        float numberOfCoats;
+        float costOfPaintPerLitre;
 
-        float roomHeightWall1 = randomNumberGenerator;  //in any dimensions
-        float roomWidthWall1 = randomNumberGenerator;
-        float roomHeightWall2 = randomNumberGenerator;
-        float roomWidthWall2 = randomNumberGenerator;
-        float roomHeightWall3 = 10;
-        float roomWidthWall3 = 12;
-        float roomHeightWall4 = 10;
-        float roomWidthWall4 = 12;
-        float ceilingWidth = 12;
-        float ceilingHeight = 12;
-        float doorHeight = 8;
-        float doorWidth = 4;
-        float windowHeight = 6;
-        float windowWidth = 5;
-        float numberOfCoats = 2;
         float squareFeetPerLitre = 20;
-        double costOfPaintPerLitre = 4.99;
 
 
-        float wall1SurfaceArea = (roomHeightWall1*roomWidthWall1);
-        float wall2SurfaceArea = (roomHeightWall2*roomWidthWall2);
-        float wall3SurfaceArea = (roomHeightWall3*roomWidthWall3);
-        float wall4SurfaceArea = (roomHeightWall4*roomWidthWall4);
-        float ceilingSurfaceArea = (ceilingHeight*ceilingWidth);
-        float doorSurfaceArea = (doorHeight*doorWidth);
-        float windowSurfaceArea = (windowHeight*windowWidth);
-        float surfaceAreaOfAllWallsAndCeiling = (wall1SurfaceArea+wall2SurfaceArea+wall3SurfaceArea+wall4SurfaceArea+ceilingSurfaceArea);
-        float areaThatIsDeducted = (doorSurfaceArea+windowSurfaceArea);
-        float finalSurfaceArea = (surfaceAreaOfAllWallsAndCeiling-areaThatIsDeducted);
-        float finalAreaNeeded =(finalSurfaceArea*numberOfCoats);
-        float resultForAreaDivBySquareFeet =(finalAreaNeeded/squareFeetPerLitre/10);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("The Height of the wall ");
+        roomHeightWall1 = scanner.nextFloat();
+
+        System.out.println("The Width of the wall ");
+        roomWidthWall1 = scanner.nextFloat();
+
+        double totalAreaOfWalls = (roomWidthWall1 * roomHeightWall1) ;
+
+
+        System.out.println("The Width of the ceiling ");
+        ceilingWidth = scanner.nextFloat();
+
+        System.out.println("The height of the ceiling ");
+        ceilingHeight = scanner.nextFloat();
+
+        double totalAreaOfCeiling = (ceilingHeight * ceilingWidth);
+
+        System.out.println("The height of the door ");
+        doorHeight = scanner.nextFloat();
+
+        System.out.println("The Width of the door ");
+        doorWidth = scanner.nextFloat();
+
+        double totalAreaOfDoor = (doorHeight * doorWidth);
+
+        System.out.println("The height of the window ");
+        windowHeight = scanner.nextFloat();
+
+        System.out.println("The Width of the window ");
+        windowWidth = scanner.nextFloat();
+
+        double totalAreaOfWindow = (windowHeight * windowWidth);
+
+        System.out.println("number coats you want to apply ");
+        numberOfCoats = scanner.nextFloat();
+
+        double areaOfEverything = ((totalAreaOfDoor+totalAreaOfWindow)-(totalAreaOfWalls + totalAreaOfCeiling));
+
+        System.out.println(areaOfEverything);
+
+
+        /*float wall1SurfaceArea = (roomHeightWall1 * roomWidthWall1);
+        float ceilingSurfaceArea = (ceilingHeight * ceilingWidth);
+        float doorSurfaceArea = (doorHeight * doorWidth);
+        float windowSurfaceArea = (windowHeight * windowWidth);
+        float surfaceAreaOfAllWallsAndCeiling = wall1SurfaceArea +ceilingSurfaceArea;
+        float areaThatIsDeducted = (doorSurfaceArea + windowSurfaceArea);
+        float finalSurfaceArea = (surfaceAreaOfAllWallsAndCeiling - areaThatIsDeducted);
+        float finalAreaNeeded = (finalSurfaceArea * numberOfCoats);
+        float resultForAreaDivBySquareFeet = (finalAreaNeeded / squareFeetPerLitre / 10);
         float roundedUp = (Math.round(resultForAreaDivBySquareFeet));
-        double costResult = (roundedUp * costOfPaintPerLitre);
+       */ //  float costResult = (roundedUp * costOfPaintPerLitre);
 
-        //Scanner scanner = new Scanner(System.in);
-       // System.out.println("what is your name ");
 
-        System.out.println("This will cost £" + costResult + " pounds" + " To paint one room");
+
     }
-}
 
+}
